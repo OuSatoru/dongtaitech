04cf6220c807122fbbe91b60a9e83c2ccbfc483e
Wang Cong <ousatoru@hotmail.com>
2016-09-20 09:31:44 +0800

    Read from json

--- BranchTown.py
+++ BranchTown.py
@@ -1,22 +1,42 @@
 import json
 
-dic = {}
+def toJSONfile():
+    '''with file branchtown.csv in directory'''
+    dic = {}
 
-with open('branchtown.csv', encoding='gbk') as file:
-    ls = file.readlines()
-for line in ls:
-    bt = line.strip('\n').split(',')
-    if bt[0] not in dic:
-        dic[bt[0]] = []
-        dic[bt[0]].append(bt[1])
-    else:
-        dic[bt[0]].append(bt[1])
+    with open('branchtown.csv', encoding='gbk') as file:
+        ls = file.readlines()
+    for line in ls:
+        bt = line.strip('\n').split(',')
+        if bt[0] not in dic:
+            dic[bt[0]] = []
+            dic[bt[0]].append(bt[1])
+        else:
+            dic[bt[0]].append(bt[1])
 
 
-out = json.dumps(dic, ensure_ascii=False)
+    out = json.dumps(dic, ensure_ascii=False)
 
-with open('branchtown.json', 'w', encoding='gbk') as file:
-    file.write(out)
+    with open('branchtown.json', 'w', encoding='gbk') as file:
+        file.write(out)
+
+text = ""
+d = json.loads(open('branchtown.json', encoding='gbk').read())
+print(type(d))
+
+with open('src.csv', encoding='gbk') as file:
+    src = file.readlines()
+for line in src:
+    sr = line.strip('\n').split(',')
+    text += sr[0]+'支行'+','
+    for cun in d[sr[0]+'支行']:
+        if cun[:2] == sr[1]:
+            text += cun + '\n'
+
+    with open('dest.csv', 'w', encoding='gbk') as file:
+        file.write(text)
+
+
 
 text = ""
 
@@ -25,9 +45,9 @@ with open('src.csv', encoding='gbk') as file:
 for line in src:
     sr = line.strip('\n').split(',')
     text += sr[0]+'支行'+','
-    for cun in dic[sr[0]+'支行']:
+    for cun in d[sr[0]+'支行']:
         if cun[:2] == sr[1]:
             text += cun + '\n'
 
-with open('dest.csv', 'w', encoding='gbk') as file:
-    file.write(text)
+    with open('dest.csv', 'w', encoding='gbk') as file:
+        file.write(text)
