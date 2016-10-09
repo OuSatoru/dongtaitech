1c32898fdf68547bf2b958497bfb6936cec1aa8e
Wang Cong <ousatoru@hotmail.com>
2016-10-09 16:27:07 +0800

    update village

--- BranchTown.py
+++ BranchTown.py
@@ -1,10 +1,11 @@
 import json
 
+
 def toJSONfile():
     '''with file branchtown.csv in directory'''
     dic = {}
 
-    with open('branchtown.csv', encoding='gbk') as file:
+    with open('SELECT_ZHMC__CMC_FROM_YGXD_ZHXXB_z_LEFT_.csv', encoding='utf8') as file:
         ls = file.readlines()
     for line in ls:
         bt = line.strip('\n').split(',')
@@ -14,12 +15,14 @@ def toJSONfile():
         else:
             dic[bt[0]].append(bt[1])
 
-
     out = json.dumps(dic, ensure_ascii=False)
 
     with open('branchtown.json', 'w', encoding='gbk') as file:
         file.write(out)
 
+
+# sharp when not needed
+# toJSONfile()
 text = ""
 d = json.loads(open('branchtown.json', encoding='gbk').read())
 print(type(d))
@@ -28,26 +31,24 @@ with open('src.csv', encoding='gbk') as file:
     src = file.readlines()
 for line in src:
     sr = line.strip('\n').split(',')
-    text += sr[0]+'支行'+','
-    for cun in d[sr[0]+'支行']:
+    text += sr[0] + '支行' + ','
+    for cun in d[sr[0] + '支行']:
         if cun[:2] == sr[1]:
             text += cun + '\n'
 
-    with open('dest.csv', 'w', encoding='gbk') as file:
-        file.write(text)
+with open('dest.csv', 'w', encoding='gbk') as file:
+    file.write(text)
 
-
-
-text = ""
+'''text = ""
 
 with open('src.csv', encoding='gbk') as file:
     src = file.readlines()
 for line in src:
     sr = line.strip('\n').split(',')
-    text += sr[0]+'支行'+','
-    for cun in d[sr[0]+'支行']:
+    text += sr[0] + '支行' + ','
+    for cun in d[sr[0] + '支行']:
         if cun[:2] == sr[1]:
             text += cun + '\n'
 
     with open('dest.csv', 'w', encoding='gbk') as file:
-        file.write(text)
+        file.write(text)'''
